e51ec750d9bffd7d31b2c03f922cde356edb190c
Aryan Kashem <aryankashem@gmail.com>
2024-03-27 12:55:40 -0400

    working tests

--- test/test_bldc_control.c
+++ test/test_bldc_control.c
@@ -17,18 +17,10 @@ void test_case_1(void) {
   TEST_ASSERT_EQUAL_INT(1, a);  // Passes
 }
 
-void test_case_2(void) {
-  bldc_6step_init(30, &jupiter_test_config);
-
-  int a = 1;
-  TEST_ASSERT_EQUAL_INT(2, a);  // Fails
-}
-
 int main(void) {
   UNITY_BEGIN();
 
   RUN_TEST(test_case_1);
-  RUN_TEST(test_case_2);
 
   UNITY_END();
 
